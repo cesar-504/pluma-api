c4ff6ae6659c14d9cf72403ac2c622994c70baea
Cesar Guerra <cesargb504@gmail.com>
2017-04-16 12:38:06 -0700

    cambio vercion js es2015

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,8 +11,8 @@ let env = process.env.NODE_ENV || 'development';
 export let settings: Config = {
   name: 'pluma-api-server',
   version: '1.0.0',
-  port: 3001,
-  env: 'dev'
+  port: process.env.PORT || 3001,
+  env: 'dev',
 };
 
 if (env === 'production') {
